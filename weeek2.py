57f6c442b7d6d11f091b27dee1aa1d780323b66a
jourdysirait <jordilukianosirait@gmail.com>
2023-03-31 16:04:05 +0700

    mmm

---
+++ weeek2.py
@@ -0,0 +1,23 @@
+  classroom = {
+      "aslab" : {
+      "aslab 1": "Safri",
+      "aslab 2": "Rian",
+      "aslab 3": "Yuki"
+      },
+      "praktikan": {
+      "baris 1" : {
+      "nama": "luqman",
+      "umur": 19
+      "nama": "ivan"
+      "umur": 18
+
+
+}
+
+
+}
+
+
+}
+    
+    
---
+++ week2.py
@@ -0,0 +1,3 @@
+Celcius = int(input("masukan nilai Celcius: "))
+Fahrenheit = (9/5) * Celcius + 32
+print(f"nilai {Celcius} C ke F adalah: {Fahrenheit}")
