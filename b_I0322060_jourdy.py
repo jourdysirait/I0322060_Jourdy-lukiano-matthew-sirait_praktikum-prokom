56f6c69173edd281f0dce8d3b23bf7419fc191f2
jourdysirait <jordilukianosirait@gmail.com>
2023-04-08 15:20:17 +0700

    file

---
+++ b_I0322060_jourdy.py
@@ -0,0 +1,21 @@
+# no 2 aplikasi penjualan rumah
+  
+
+
+# report 1 yang harus dibuat
+  utang = float(input("masukan jumlah utang "))
+  bunga = float(input("masukan suku bunga tahunan:"))
+  lama_pinjaman = float(input("masukan lama pinjaman dalam tahun:"))
+
+
+
+
+#output
+print('cicilan pokok bulanan = ', cicilan_pokok_bulanan)
+print('cicilan pokok bulanan= ', cicilan_pokok_bulanan)
+print('tota cicilan setiap bulan = ',total_cicilan_bulan)
+
+# report yang harus dibuat total pembayaran pertama yang harus disiapkan calon pembeli
+harga_rumah = float(input("masukan harga rumah"))
+cicilan_bulan_pertama = float(input("masukan cicilan bulan pertama"))
+biaya_notaris = float(input("masukan biaya notaris"))
